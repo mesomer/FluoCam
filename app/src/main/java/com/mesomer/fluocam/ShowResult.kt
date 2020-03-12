6e1f6a8696b24d80f2acd7f73412095d093ba7b9
mesomer <42628863+mesomer@users.noreply.github.com>
2020-03-12 18:43:23 +0800

    在新线程查询数据库

--- app/src/main/java/com/mesomer/fluocam/ShowResult.kt
+++ app/src/main/java/com/mesomer/fluocam/ShowResult.kt
@@ -1,9 +1,15 @@
 package com.mesomer.fluocam
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import android.graphics.Point
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+import android.view.View
 import android.widget.ExpandableListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.mikephil.charting.charts.LineChart
 import com.github.mikephil.charting.data.Entry
@@ -12,65 +18,120 @@ import com.github.mikephil.charting.data.LineDataSet
 import com.mesomer.databasetest.data.AppDatabase
 import com.mesomer.databasetest.data.MyDAO
 import com.mesomer.fluocam.adapter.MyExpandableAdapter
-import org.w3c.dom.Entity
-import java.security.KeyStore
+import java.util.*
+import kotlin.collections.ArrayList
 
 
 class ShowResult : AppCompatActivity() {
 
-    private var db: AppDatabase? = null
-    private var myDao: MyDAO? = null
+    private lateinit var db : AppDatabase
+    private lateinit var myDao: MyDAO
+    private lateinit var myThread: MyThread
+    private lateinit var chart: LineChart
+    private lateinit var expandListView: ExpandableListView
+
+    private lateinit var groupList: List<String>
+
+    internal inner class MyThread : Thread() {
+        lateinit var mHandler: Handler
+        override fun run() {
+            Looper.prepare()
+            mHandler = @SuppressLint("HandlerLeak")
+            object : Handler() {
+                override fun handleMessage(msg: Message?) {
+                    super.handleMessage(msg)
+                    if (msg?.what == 0x222) {
+                        //读图
+                        //分类
+                        runOnUiThread {
+                            //画统计图
+                        }
+                    }
+                    if (msg?.what == 0x111) {
+                        groupList = myDao!!.getAllGroupID()
+                        var groupArrayList = ArrayList<String>()
+                        var sampleArrayList = ArrayList<ArrayList<String>>()
+                        //从数据库读信息
+                        for (groupid in groupList) {
+                            groupArrayList.add(groupid)
+                            var photoReturn = myDao!!.getPhotoByGroupID(groupid)
+                            var cocentrationArray = ArrayList<String>()
+                            for (photo in photoReturn) {
+                                cocentrationArray.add(photo.concentration)
+                            }
+                            sampleArrayList.add(cocentrationArray)
+                        }
+                        runOnUiThread {
+                            val adapter = MyExpandableAdapter(
+                                this@ShowResult,
+                                groupArrayList,
+                                sampleArrayList
+                            )
+                            expandListView.setAdapter(adapter)
+                        }
+                    }
+                }
+            }
+            Looper.loop()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_result)
+        expandListView = findViewById(R.id.expand_list)
+        chart = findViewById(R.id.chart)
 
-        val expandListView = findViewById<ExpandableListView>(R.id.expand_list)
-        val chart = findViewById<LineChart>(R.id.chart)
+        db = AppDatabase.getAppDataBase(context = this)!!
+        myDao= db.myDao()
 
+        myThread = MyThread()
+        myThread.start()
+
+        expandListView.setOnGroupExpandListener { groupPosition -> sendmessage(0x222)  }
         //设置图表View的高
+        setChartViewSize(chart)
+        //延时一段时间更新UI
+        Timer().schedule(object : TimerTask(){
+            override fun run() {
+                sendmessage(0x111)
+            }
+        },100)
+
+
+        //画图
+        drawLine()
+
+    }
+
+    private fun setChartViewSize(view: View) {
         val myDisplay = windowManager.defaultDisplay
-        var chartParam = chart.layoutParams
+        var viewParam = view.layoutParams
         var point = Point(0, 0)
         myDisplay.getSize(point)
-        chartParam.height = point.y / 2
-        //数据库
-        db = AppDatabase.getAppDataBase(context = this)
-        myDao = db?.myDao()
-        //展开表的内容
-        var groupList = myDao!!.getAllGroupID()
-        var groupArrayList = ArrayList<String>()
-        var sampleArrayList = ArrayList<ArrayList<String>>()
-        //从数据库读信息
-        for (groupid in groupList) {
-            groupArrayList.add(groupid)
-            var photoReturn = myDao!!.getPhotoByGroupID(groupid)
-            var cocentrationArray = ArrayList<String>()
-            for (photo in photoReturn) {
-                cocentrationArray.add(photo.concentration)
-            }
-            sampleArrayList.add(cocentrationArray)
-        }
-        //配置BaseExpandableListAdapter
-        val adapter = MyExpandableAdapter(this, groupArrayList, sampleArrayList)
-        expandListView.setAdapter(adapter)
-        //画图
-        var mydata = ArrayList<Pair<Float,Float>>()
-        mydata.add(Pair(0f,0f))
-        mydata.add(Pair(1f,1f))
-        mydata.add(Pair(2f,2f))
-        mydata.add(Pair(5f,5f))
-        mydata.add(Pair(10f,15f))
+        viewParam.height = point.y / 2
+    }
+    private fun sendmessage(msgnum:Int){
+        val msg = Message()
+        msg.what = 0x111
+        myThread.mHandler.sendMessage(msg)
+    }
+    fun drawLine(){
+        var mydata = ArrayList<Pair<Float, Float>>()
+        mydata.add(Pair(0f, 0f))
+        mydata.add(Pair(1f, 1f))
+        mydata.add(Pair(2f, 2f))
+        mydata.add(Pair(5f, 5f))
+        mydata.add(Pair(10f, 15f))
         var entries = ArrayList<Entry>()
-        for(point in mydata){
-            entries.add(Entry(point.first,point.second))
+        for (point in mydata) {
+            entries.add(Entry(point.first, point.second))
         }
-        var datset=LineDataSet(entries,"Lable")
+        var datset = LineDataSet(entries, "Lable")
         datset.setColor(Color.BLACK)
-        var linedata=LineData(datset)
-        chart.data=linedata
+        var linedata = LineData(datset)
+        chart.data = linedata
         chart.invalidate()
-
-
     }
 
 }
